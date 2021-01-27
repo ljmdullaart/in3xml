47cda7ae0f5df75c82cf2adf257d0be292ccce6c
ljmdullaart <ljm@acm.org>
2021-01-27 14:26:31 +0100

    Generate table for better indents.

---
+++ mkfontwidthtable.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#INSTALL@ /usr/local/bin/mkfontwidthtable
+tmpfont=tmpfont
+
+font=$(basename $1)
+if [ ! -f "/usr/share/groff/current/font/devps/$font" ] ; then
+	echo "No suchfont $font"
+	exit 
+fi
+
+cp "/usr/share/groff/current/font/devps/$font" $tmpfont
+sed -i '1,/charset/d' $tmpfont
+sed -i 's/,.*//' $tmpfont
+sed -i 's/^\(..\)\t/\\(\1  /'  $tmpfont
+sed -i 's/^\(...\)\t/\\[\1]  /'  $tmpfont
+sed -i '/^\(.....*\)\t/d'  $tmpfont
+sed -i '/"/d'  $tmpfont
+sed -i 'a .br'  $tmpfont
+groff $tmpfont > $tmpfont.ps
+
+echo "#INSTALL@ /usr/local/share/in3/$font.measure" > $font.measure
+ps2ascii $tmpfont.ps  | sed 's/ //g;s/^\(.\)/\1	/'>>$font.measure
+sudo cp $font.measure /usr/local/share/in3/
+rm $tmpfont
