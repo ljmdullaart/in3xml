dfaa71ce21577f5991d00f465a6d64f3557a68e7
ljmdullaart <ljm@acm.org>
2021-02-13 19:02:06 +0100

    minor changes

--- charmap2in.pl
+++ charmap2in.pl
@@ -8,20 +8,27 @@ if (open(my $CH,'<','in3charmap1')){
 else {die "Cannot open in3charmap1";}
 
 
+my $i=5;
 print ".header\n\n";
 print ".h1 In3 character map\n\n";
 print ".h2 Percent translations\n\n";
 print "	.b %% +	.b char	.b groff	.b html\n";
 for (@charmap){
+	if ($i==0){
+		print "\n\n.page\n\n";
+		print "	.b %% +	.b char	.b groff	.b html\n";
+	}
 	if (/^%([^	]*)	/){
 		print "	$1	";
 		print;
+		$i++;
+		if ($i>35){$i=0;}
 	}
 }
 print "\n\n";
 print ".page\n\n";
 print ".h2 Characterset\n\n";
-my $i=3;
+$i=3;
 for (@charmap){
 	if (/^%([^	]*)	/){}
 	elsif (/^#/){}
