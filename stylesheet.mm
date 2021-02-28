22ae3449098c6bc52e1db8f68fbd6d390026f333
ljmdullaart <ljm@acm.org>
2021-02-28 17:43:28 +0100

    stylesheets for the documentation.

---
+++ stylesheet.mm
@@ -0,0 +1,13 @@
+.nr Ej 1
+.ds HF  HB HB HI HI I I I
+.ds HP  16 12 12  0 0 0 0
+.nr Hb 4
+.nr Hs 1
+.nr Hps 0
+.ds pg*header '' in3 ''
+.ds pg*footer ''- \\nP -''
+.ev classtest
+.ll 6i
+.in 3i
+.gcolor red
+.ev
