baed8d630e18fec72010e4629d46389aa0689f85
ljmdullaart <ljm@acm.org>
2023-08-28 17:53:31 +0200

    only mv if pdftk succeeds

--- pdfrm1stblank.sh
+++ pdfrm1stblank.sh
@@ -5,6 +5,7 @@ IN="$1"
 TMP=/tmp/pdfrmblank.$$
 PERCENT=$(gs -o -  -dFirstPage=1 -dLastPage=1 -sDEVICE=inkcov "$IN" | grep CMYK | nawk 'BEGIN { sum=0; } {sum += $1 + $2 + $3 + $4;} END { printf "%.5f\n", sum } ')
 if [ $(echo "$PERCENT < 0.001" | bc) -eq 1 ] ; then
-	pdftk "$IN" cat 2-end output "$TMP"
-	mv "$TMP" "$IN"
+	if pdftk "$IN" cat 2-end output "$TMP" ; then
+		mv "$TMP" "$IN"
+	fi
 fi
